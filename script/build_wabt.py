47abba6c94ecf74c3c173344684891f2ca29c2e3
mohanson <mohanson@outlook.com>
2026-09-18 10:36:24 +0800

    Update wabt to v1.0.42

--- script/build_wabt.py
+++ script/build_wabt.py
@@ -23,7 +23,7 @@ def cd(dst: str) -> typing.Generator[None, typing.Any, None]:
 root = os.path.dirname(os.path.dirname(__file__))
 os.chdir(root)
 
-version = '1.0.41'
+version = '1.0.42'
 url = ''
 match platform.system().lower():
     case 'darwin':
